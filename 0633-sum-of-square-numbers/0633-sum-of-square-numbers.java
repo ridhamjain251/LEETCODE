014dd90aa5e6c9577f3d860e7b8de4064f42a2cc
ridhamjain251 <ridhamjain0512@gmail.com>
2026-08-13 23:50:54 +0530

    Time: 4 ms (82.45%), Space: 42.3 MB (32.15%) - LeetHub

---
+++ 0633-sum-of-square-numbers/0633-sum-of-square-numbers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public boolean judgeSquareSum(int c) {
+        long l=0;
+     long r=(long)Math.sqrt(c);
+        while(l<=r){
+            long s= l*l+r*r;
+            if(s==c)
+            return true;
+            else if(s<c)
+            l++;
+            else r--;
+                   }
+                   return false;
+        
+
+        
+    }
+}
