b0f82ebfa5933961c46dda0d389f5b5a7fb9ffc7
ridhamjain251 <ridhamjain0512@gmail.com>
2026-08-20 16:08:59 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.95%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int fib(int n) {
+        int a=0,b=1,sum=0;
+        if(n<=1)
+        return n;
+        for(int i=1;i<n;i++){
+            sum=a+b;
+            a=b;
+            b=sum;
+        }
+        return b;
+
+        
+    }
+}
