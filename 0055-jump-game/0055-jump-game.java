a915244f00ca470b999ff3addb188c8a12990eeb
ridhamjain251 <ridhamjain0512@gmail.com>
2026-09-03 10:53:16 +0530

    Time: 2 ms (89.02%), Space: 48 MB (42.88%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.java
@@ -0,0 +1,13 @@
+class Solution {
+    public boolean canJump(int[] nums) {
+        int reach=0;
+        for(int i=0; i<nums.length; i++){
+            if(reach<i)
+            return false;
+            else
+            reach= Math.max(reach,nums[i]+i);
+        }
+        return true;
+        
+    }
+}
